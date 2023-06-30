203df3baa68a02413377bb8aa369bc887c6afe8f
willian k. souza <willianksouza94@gmail.com>
2023-06-29 22:42:41 -0300

    a ultima aula foi de sequelize

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
 
@@ -6,11 +7,26 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+
+
+
 app.get('/', (req, res) => {
     res.render('index')
 })
 app.get('/perguntar', (req, res) => {
     res.render('perguntar')
+})
+
+app.post('/salvarpergunta', (req, res) => {
+    const titulo = req.body.titulo
+    const descricao = req.body.descricao
+    console.log(typeof titulo)
+    res.send(`${titulo} ${descricao}`)
 })
 
 app.listen(8080, () =>{
